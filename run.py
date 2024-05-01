de3a6395cde4a7812a258edec4ed6b2ee5835f77
Katherine Weston <119933259+Katherine-Holland@users.noreply.github.com>
2024-05-01 10:52:58 +0000

    Calculate surplus data

--- run.py
+++ run.py
@@ -59,16 +59,20 @@ def update_sales_data(data):
   sales_worksheet.append_row(data)
   print("Sales worksheet updated successfully!\n")
 
-
 def calculate_surplus_data(sales_row):
-  """
-  Compare surplus stock
-  """
-  print("Calculating surplus data...\n")
-  stock = SHEET.worksheet("stock").get_all_values()
-  stock_row = stock[-1]
-  print(stock_row)
+    """
+    Compare surplus stock
+    """
+    print("Calculating surplus data...\n")
+    stock = SHEET.worksheet("stock").get_all_values()
+    stock_row = stock[-1]
 
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+    return surplus_data
+   
 
 def main():
   """
@@ -77,7 +81,9 @@ def main():
   data = get_sales_data()
   sales_data = [int(num) for num in data]
   update_sales_data(sales_data)
-  calculate_surplus_data(sales_data)
+  new_surplus_data = calculate_surplus_data(sales_data)
+  print(new_surplus_data)
+  
 
 
 print("welcome to love sandwiches")
